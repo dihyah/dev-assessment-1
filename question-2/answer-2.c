642d516429041cc645d789d0fdd58feac4f12f13
dihyah <hiidihyah@gmail.com>
2021-10-16 00:11:23 +0800

    answered question 2

--- question-2/answer-2.c
+++ question-2/answer-2.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 
-int main(void) {
+int main(void){
   int n;
 
   printf("Enter a number for n: ");
   scanf("%i", &n);
 
-  for (int i = 0; i <= n; i++){
-    for (int j = 1; j <= i; j++)
-
-      printf("%i", j);
-      printf("\n");
+  for (int i = 1; i <= n; i++){
+    for (int j = 1; j <= i; j++){
+      if (i-1 == j)
+        printf("*");
+      else
+        printf("%i", j);
+    }
+    printf("\n");
   }
 }
